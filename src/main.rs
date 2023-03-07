75d8befecb82bf3d22e997ee2c53838b3033a18e
Phineas Jensen <phin@zayda.net>
2023-03-07 11:09:26 -0700

    Add triangle interesctions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate nalgebra as na;
 
+use std::num;
+
 use na::Vector3;
 
 type Vec3 = Vector3<f64>;
@@ -14,6 +16,16 @@ struct Camera {
 struct Sphere {
     center: Vec3,
     radius: f64,
+    k_d: f64,
+    k_s: f64,
+    k_a: f64,
+    k_gls: f64,
+    o_d: Vec3,
+    o_s: Vec3,
+}
+
+struct Triangle {
+    vertices: [Vec3; 3],
     k_d: f64,
     k_s: f64,
     k_a: f64,
@@ -37,7 +49,7 @@ trait Object {
     /// closer objects, letting this returning None if there's a closer intersection.
     fn intersected_by(&self, ray: &Ray, t_closest: &mut f64) -> Option<Vec3>;
 
-    fn color(&self, normal: Vec3, view_direction: Vec3, scene: &Scene) -> Vec3;
+    fn color(&self, point: Vec3, view_direction: Vec3, scene: &Scene) -> Vec3;
 
     fn normal(&self, point: Vec3) -> Vec3;
 }
@@ -115,6 +127,85 @@ impl Object for Sphere {
     }
 }
 
+impl Object for Triangle {
+    fn intersected_by(&self, ray: &Ray, t_closest: &mut f64) -> Option<Vec3> {
+        let normal = self.normal(Vec3::zeros());
+        let d = -(normal.dot(&self.vertices[0]));
+        let v_d = normal.dot(&ray.direction.normalize());
+        if v_d == 0.0 {
+            return None;
+        }
+        let v_o = -(normal.dot(&ray.origin) + d);
+        let t = v_o / v_d;
+        if t < 0.0 || *t_closest < t {
+            return None;
+        }
+        let dominant_index = normal.iamax();
+        let r = ray.at(t).remove_row(dominant_index);
+        let pv = self.vertices.map(|v| v.remove_row(dominant_index) - r);
+        let mut num_crossings = 0;
+        for i in 0..pv.len() {
+            let next = (i + 1) % pv.len();
+            let signholder = if pv[i].y < 0.0 { -1 } else { 1 };
+            let next_signholder = if pv[next].y < 0.0 { -1 } else { 1 };
+            if signholder != next_signholder {
+                if pv[i].x > 0.0 && pv[next].x > 0.0 {
+                    num_crossings += 1;
+                } else if pv[i].x > 0.0 || pv[next].x > 0.0 {
+                    let u_cross =
+                        pv[i].x - pv[i].y * (pv[next].x - pv[i].x) / (pv[next].y - pv[i].y);
+                    if u_cross > 0.0 {
+                        num_crossings += 1;
+                    }
+                }
+            }
+        }
+
+        match num_crossings % 2 == 1 {
+            false => None,
+            true => {
+                *t_closest = t;
+                Some(ray.at(t))
+            }
+        }
+    }
+
+    fn color(&self, point: Vec3, view_direction: Vec3, scene: &Scene) -> Vec3 {
+        let normal = &self.normal(point);
+        let v = -view_direction;
+        let dl = scene.direction_to_light.normalize();
+        let ndl = normal.dot(&dl);
+        let r = 2.0 * normal * normal.dot(&dl) - dl;
+
+        let ambient = self.k_a * scene.ambient_light.component_mul(&self.o_d);
+        let diffuse = self.k_d * scene.light_color.component_mul(&self.o_d) * ndl.max(0.0);
+        let specular = self.k_s
+            * scene.light_color.component_mul(&self.o_s)
+            * v.dot(&r).max(0.0).powf(self.k_gls);
+
+        //let shadow_ray = Ray {
+        //    origin: point + normal,
+        //    direction: (scene.direction_to_light - point).normalize(),
+        //};
+        //if let Some((_, _, t_closest)) = shadow_ray.nearest_hit(scene) {
+        //    if t_closest > 0.0000001 {
+        //        return Vec3::new(0.0, 0.0, 0.0);
+        //    }
+        //}
+
+        let res = if ndl > 0.0 {
+            ambient + diffuse + specular
+        } else {
+            ambient + diffuse
+        };
+        res
+    }
+
+    fn normal(&self, _point: Vec3) -> Vec3 {
+        (self.vertices[1] - self.vertices[0]).cross(&(self.vertices[2] - self.vertices[1]))
+    }
+}
+
 struct Ray {
     origin: Vec3,
     direction: Vec3,
@@ -146,11 +237,11 @@ impl Ray {
     }
 }
 
-const ASPECT: f64 = 16.0 / 9.0;
+const ASPECT: f64 = 1.0;
 const WIDTH: f64 = 600.0;
 const HEIGHT: f64 = WIDTH / ASPECT;
 
-const VIEWPORT_WIDTH: f64 = 3.0;
+const VIEWPORT_WIDTH: f64 = 2.0;
 const VIEWPORT_HEIGHT: f64 = VIEWPORT_WIDTH / ASPECT;
 const FOCAL_LENGTH: f64 = 1.0;
 
@@ -164,16 +255,54 @@ fn print_color(color: &Vec3) {
 }
 
 fn main() {
-    let scene_1_objects: Vec<Box<dyn Object>> = vec![Box::new(Sphere {
-        center: Vec3::new(0.0, 0.0, 0.0),
-        radius: 0.4,
-        k_d: 0.7,
-        k_s: 0.2,
-        k_a: 0.1,
-        o_d: Vec3::new(1.0, 0.0, 1.0),
-        o_s: Vec3::new(1.0, 1.0, 1.0),
-        k_gls: 16.0,
-    })];
+    let scene_1_objects: Vec<Box<dyn Object>> = vec![
+        Box::new(Sphere {
+            center: Vec3::new(0.5, 0.0, -0.15),
+            radius: 0.05,
+            k_d: 0.8,
+            k_s: 0.1,
+            k_a: 0.3,
+            o_d: Vec3::new(1.0, 1.0, 1.0),
+            o_s: Vec3::new(1.0, 1.0, 1.0),
+            k_gls: 4.0,
+        }),
+        Box::new(Sphere {
+            center: Vec3::new(0.3, 0.0, -0.1),
+            radius: 0.08,
+            k_d: 0.8,
+            k_s: 0.8,
+            k_a: 0.1,
+            o_d: Vec3::new(1.0, 0.0, 0.0),
+            o_s: Vec3::new(0.5, 1.0, 0.5),
+            k_gls: 32.0,
+        }),
+        Box::new(Triangle {
+            vertices: [
+                Vec3::new(0.3, -0.3, -0.4),
+                Vec3::new(0.0, 0.3, -0.1),
+                Vec3::new(-0.3, -0.3, 0.2),
+            ],
+            k_d: 0.9,
+            k_s: 0.9,
+            k_a: 0.1,
+            o_d: Vec3::new(0.0, 0.0, 1.0),
+            o_s: Vec3::new(1.0, 1.0, 1.0),
+            k_gls: 32.0,
+        }),
+        Box::new(Triangle {
+            vertices: [
+                Vec3::new(-0.2, 0.1, 0.1),
+                Vec3::new(-0.2, -0.5, 0.2),
+                Vec3::new(-0.2, 0.1, -0.3),
+            ],
+            k_d: 0.9,
+            k_s: 0.5,
+            k_a: 0.1,
+            o_d: Vec3::new(1.0, 1.0, 0.0),
+            o_s: Vec3::new(1.0, 1.0, 1.0),
+            k_gls: 4.0,
+        }),
+    ];
     let scene_2_objects: Vec<Box<dyn Object>> = vec![
         Box::new(Sphere {
             center: Vec3::new(0.45, 0.0, -0.15),
@@ -280,9 +409,9 @@ fn main() {
     ];
 
     let scene_1 = Scene {
-        direction_to_light: Vec3::new(0.0, 1.0, 0.0),
+        direction_to_light: Vec3::new(1.0, 0.0, 0.0),
         light_color: Vec3::new(1.0, 1.0, 1.0),
-        ambient_light: Vec3::new(0.0, 0.0, 0.0),
+        ambient_light: Vec3::new(0.1, 0.1, 0.1),
         background_color: Vec3::new(0.2, 0.2, 0.2),
         objects: scene_1_objects,
     };
@@ -316,7 +445,7 @@ fn main() {
                 origin: camera_origin,
                 direction: lower_left + u * horizontal + v * vertical - camera_origin,
             };
-            let color = r.color(&scene_2);
+            let color = r.color(&scene_1);
             print_color(&color);
         }
         println!("");
